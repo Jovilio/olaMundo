393e8c0c55c232d20d4f8529b8d334a50bc313e0
Jovilio <famosojovilio@gmail.com>
2024-11-05 18:36:45 +0100

    actualização geral
    mudança de todos os arquivos, adicionada a responsividade

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-
 window.addEventListener('scroll', function () {
-    // Obter a posição do scroll vertical
-    const scrollY = window.scrollY || document.documentElement.scrollTop;
-    var s2 = document.querySelector('.s2');
+    const scrollY = window.scrollY; // Posição do scroll
 
-    if (scrollY >= 100) {
-        // console.log("aparecer sessão 1");
-        // s2.style.background = "green";
-        // s2.setAttribute('display', 'flex')
-    } else {
 
-        // s2.style.background = "#68001a";
+    const h1 = document.getElementById('nome');
+    const h2 = document.getElementById('cargo');
 
-    }
-    // Obter a posição do scroll horizontal (caso necessário)
-    // const scrollX = window.scrollX || document.documentElement.scrollLeft;
+    const tamanhoInicialH1 = 60; // em pt
+    const tamanhoInicialH2 = 30; // em pt
+    const tamanhoMinimoH1 = 12; // em pt
+    const tamanhoMinimoH2 = 10; // em pt
 
-    // console.log(`Posição do scroll vertical: ${scrollY}`);
-    // console.log(`Posição do scroll horizontal: ${scrollX}`);
+    const fatorH1 = (tamanhoInicialH1 - tamanhoMinimoH1) / 360;
+    const fatorH2 = (tamanhoInicialH2 - tamanhoMinimoH2) / 360;
+
+    const novoTamanhoH1 = Math.max(tamanhoInicialH1 - scrollY * fatorH1, tamanhoMinimoH1);
+    const novoTamanhoH2 = Math.max(tamanhoInicialH2 - scrollY * fatorH2, tamanhoMinimoH2);
+
+    h1.style.fontSize = `${novoTamanhoH1}pt`;
+    h2.style.fontSize = `${novoTamanhoH2}pt`;
 });
+
+
+const boasVindas = document.querySelector('.boasVindas');
+const mensagens = [
+    'Olá Mundo',
+    'Role para baixo',
+    
+];
+let indice = 0;
+
+setInterval(() => {
+    // Define o conteúdo da mensagem atual
+    boasVindas.textContent = mensagens[indice];
+
+    // Incrementa o índice para a próxima mensagem
+    indice = (indice + 1) % mensagens.length; // Reinicia para 0 ao chegar ao fim do array
+}, 5000); // Intervalo de 2000ms (2 segundos)
 
